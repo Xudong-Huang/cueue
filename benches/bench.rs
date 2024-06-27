bbc3cc6af5bd5c3d8b7fb0a0427d2d26034586e1
Xudong Huang <huangxu008@hotmail.com>
2024-06-27 13:45:09 +0800

    :pencil: refine bench

--- benches/bench.rs
+++ benches/bench.rs
@@ -9,12 +9,14 @@ use self::test::Bencher;
 fn bench_write(b: &mut Bencher) {
     let (mut w, mut r) = cueue(16).unwrap();
 
-    let data = &b"123456789abcdefh"[..];
+    let data = &b"123456789abcdefhelloworld"[..];
     let data_len = data.len();
 
     let rt = std::thread::spawn(move || {
         while !r.is_abandoned() {
             let _rr = r.read_chunk();
+            // let len = _rr.len();
+            // r.commit_read(len);
             r.commit();
         }
     });
@@ -27,9 +29,8 @@ fn bench_write(b: &mut Bencher) {
             }
             std::hint::spin_loop();
         };
-        unsafe {
-            std::ptr::copy_nonoverlapping(data.as_ptr(), buf.as_mut_ptr(), data_len);
-        }
+        // unsafe { std::ptr::copy_nonoverlapping(data.as_ptr(), buf.as_mut_ptr(), data_len) };
+        buf[..data_len].copy_from_slice(data);
         w.commit(data_len);
     });
 
